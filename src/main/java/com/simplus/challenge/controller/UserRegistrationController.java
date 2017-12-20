74b46b5beddedb00045301421aae57eed3e3a988
Diego.Souza.Dev <diego.souza.dev@gmail.com>
2017-12-20 21:57:59 -0200

    missing / in the add endpoint

--- src/main/java/com/simplus/challenge/controller/UserRegistrationController.java
+++ src/main/java/com/simplus/challenge/controller/UserRegistrationController.java
@@ -15,34 +15,34 @@ import com.simplus.challenge.service.UserService;
 @RestController
 @RequestMapping(value = "/user")
 public class UserRegistrationController {
-
-	private final UserService userService;
 	
+	private final UserService userService;
+
 	@Autowired
 	public UserRegistrationController(final UserService userSvc) {
 		this.userService = userSvc;
 	}
-
-	@RequestMapping(value = "add", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+	
+	@RequestMapping(value = "/add", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
 	public Long createUser(@RequestBody final User user) {
 		return userService.create(user);
 	}
-
+	
 	@RequestMapping(value = "/update", method = RequestMethod.PUT, consumes = "application/json", produces = "application/json")
 	public User updateUser(@RequestBody final User user) {
 		return userService.updateUser(user);
 	}
-	
+
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
 	public void delete(@PathVariable("id") final Long id) {
 		userService.delete(id);
 	}
-
+	
 	@RequestMapping(value = "/find/{id}", method = RequestMethod.GET)
 	public User readUserById(@PathVariable("id") final Long id) {
 		return userService.findUserByID(id);
 	}
-
+	
 	@RequestMapping(value = "/find", method = RequestMethod.GET)
 	public List<User> findAll() {
 		return userService.findAll();
