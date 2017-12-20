b4216fb7659b8f060f2ad98b66a3fd13183a3fa6
Diego.Souza.Dev <diego.souza.dev@gmail.com>
2017-12-20 17:04:33 -0200

    Controller Unit Tests up and running

--- src/test/java/com/simplus/challenge/controller/UserRegistrationControllerTest.java
+++ src/test/java/com/simplus/challenge/controller/UserRegistrationControllerTest.java
@@ -2,9 +2,13 @@ package com.simplus.challenge.controller;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -22,14 +26,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.simplus.challenge.enumerator.Currency;
-import com.simplus.challenge.enumerator.LanguageAndRegion;
-import com.simplus.challenge.enumerator.Ocupation;
-import com.simplus.challenge.enumerator.Role;
-import com.simplus.challenge.enumerator.State;
-import com.simplus.challenge.enumerator.Target;
-import com.simplus.challenge.model.Address;
-import com.simplus.challenge.model.Company;
 import com.simplus.challenge.model.User;
 import com.simplus.challenge.service.UserService;
 
@@ -65,56 +61,57 @@ public class UserRegistrationControllerTest {
 	@Test
 	public void testCreateUser() throws Exception {
 		final ObjectMapper mapper = new ObjectMapper();
-		
-		final User user = getUser(1l);
-		
+		final User user = new User();
 		final String json = mapper.writeValueAsString(user);
 		
 		when(service.create(new User())).thenReturn(1l);
 		
-		mockMvc.perform(MockMvcRequestBuilders.post("/user/add").content(json).accept(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().isOk())
-				.andExpect(content().string(equalTo("1")));
+		mockMvc.perform(MockMvcRequestBuilders.post("/user/add").contentType(MediaType.APPLICATION_JSON).content(json)
+				.accept(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().isOk()).andExpect(content().string(equalTo("1")));
 		
-		Mockito.verify(service, times(1)).create(new User());
+		Mockito.verify(service, times(1)).create(user);
+	}
+
+	@Test
+	public void testUpdateUser() throws Exception {
+		final ObjectMapper mapper = new ObjectMapper();
+		final User user = new User();
+		final String json = mapper.writeValueAsString(user);
+		
+		when(service.updateUser(user)).thenReturn(user);
+		
+		mockMvc.perform(MockMvcRequestBuilders.put("/user/update").contentType(MediaType.APPLICATION_JSON).content(json)
+				.accept(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().isOk());
+		
+		Mockito.verify(service, times(1)).updateUser(user);
+	}
+
+	@Test
+	public void testDelete() throws Exception {
+		
+		Mockito.doNothing().when(service).delete(1l);
+		
+		mockMvc.perform(MockMvcRequestBuilders.delete("/user/delete/1").accept(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().isOk());
+		
+		verify(service, times(1)).delete(1l);
 	}
 	
-	private User getUser(final Long index) {
-		final User user = new User();
-		user.setId(index);
-		user.setName("name" + index);
-		user.setLastName("lastname" + index);
-		user.setUserName(user.getUserName() + user.getLastName());
-		user.setEmail(user.getName() + "@domain.com");
-		user.setRole(Role.ADMIN);
-		user.setPhone("phoneNumber + index");
-		user.setRamal("ramal" + index);
-		user.setCellphone("cellNumber" + index);
-		user.setOcupation(Ocupation.MANAGER);
-		user.setCompany(getCompany(index));
-		user.setAddress(getAddress(index));
-		user.setLanguageAndRegion(LanguageAndRegion.PT_BR);
-		user.setCurrency(Currency.BRL);
-		user.setTarget(Target.VET);
-		return user;
-	}
-
-	private Company getCompany(final Long index) {
-		final Company company = new Company();
-		company.setId(index);
-		company.setName("CompanyName" + index);
-		return company;
-	}
-
-	private Address getAddress(final Long index) {
-		final Address addr = new Address();
-		addr.setStreet("Street " + index);
-		addr.setNumber("11" + index);
-		addr.setNeighborhood("neighborhood" + index);
-		addr.setCity("city" + index);
-		addr.setState(State.SC);
-		addr.setCountry("country" + index);
-		addr.setCep("89231-11" + index);
-		return addr;
+	@Test
+	public void testFindAll() throws Exception {
+		final ObjectMapper mapper = new ObjectMapper();
+		final User user1 = new User();
+		final User user2 = new User();
+		final List<User> userList = Arrays.asList(user1, user2);
+		
+		final String json = mapper.writeValueAsString(userList);
+		
+		when(service.findAll()).thenReturn(userList);
+		
+		mockMvc.perform(MockMvcRequestBuilders.get("/user/find").accept(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().isOk())
+				.andExpect(content().string(equalTo(json)));
+		
+		verify(service, times(1)).findAll();
+		
 	}
 	
 }
